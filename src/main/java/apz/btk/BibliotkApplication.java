d9ed5207c3bc40543356eb74cda1a4ee5300621c
arturisimo <arturisimo@gmail.com>
2020-01-08 22:07:39 +0100

    oauth jdbc

--- src/main/java/apz/btk/BibliotkApplication.java
+++ src/main/java/apz/btk/BibliotkApplication.java
@@ -1,15 +1,45 @@
 package apz.btk;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class BibliotkApplication extends WebMvcConfigurerAdapter {
+	
+	@Value("${spring.datasource.driver-class-name}")
+	private String driverClassName;
+	
+	@Value("${spring.datasource.url}")
+	private String url;
+	
+	@Value("${spring.datasource.username}")
+	private String user;
+	
+	@Value("${spring.datasource.password}")
+	private String password;
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BibliotkApplication.class, args);
 	}
+		
+	@Bean
+    public DataSource dataSource() {
+        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url);
+        dataSource.setUsername(user);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+		
+	
 }
--- src/main/java/apz/btk/config/SecurityConfig.java
+++ src/main/java/apz/btk/config/SecurityConfig.java
@@ -1,12 +1,17 @@
 package apz.btk.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
@@ -14,9 +19,27 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	
 	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-	  auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
+	private DataSource dataSource;
+	
+//	@Autowired
+//	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//	  auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
+//	}
+	
+	
+	/** definicion de roles y usuario. autenticacion por acceso a BBDD por jdbc */
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		/* auth con usuario en BBDD */ 
+		auth.jdbcAuthentication()
+				.passwordEncoder(encoder())
+				.dataSource(dataSource)
+	    		.usersByUsernameQuery("select usuario, password, enabled from usuarios where usuario=?")
+	    		.authoritiesByUsernameQuery("select usuario, authority from roles r, usuarios u where r.id_usuario=u.id and usuario=?");
+		
 	}
 	
 	
@@ -30,4 +53,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	    	.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).permitAll().logoutSuccessUrl("/");
 	
 	}
+	
+	@Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder(11);
+	}
+	
 }
